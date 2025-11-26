ff6bb1ac35e830e4929bf7c273b99f275da16592
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-11-26 12:15:24 +0600

    task 1 done

---
+++ task1.sql
@@ -0,0 +1,4 @@
+--! 1. Retrieve All Students' Names and Emails
+-- Write a query to retrieve the first name, last name, and email of all students from the Students table.
+
+SELECT first_name, last_name, email FROM students;
